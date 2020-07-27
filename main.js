7cbb68ba1af2b04967a30fd9df1fe69a4389e40a
Ciepiel <patrycja.ciepielewska@gmail.com>
2020-07-27 21:32:45 +0100

    Create variables selecting main HTML elements

---
+++ main.js
@@ -0,0 +1,4 @@
+//variables selecting HTML elements
+const clear = document.querySelector(".clear"); //selects the clear button
+const dateElement = document.querySelector(".list"); //selects the task list
+const input = document.getElementById("input"); //selects the input bar
